ff8034f64a0178480b71e34b50f0abb62ab51a76
geixco <68980781+geixco@users.noreply.github.com>
2020-12-05 17:42:42 -0500

    Added error handling

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -1,5 +1,13 @@
 module.exports.run = async (client, message, args) => {
     const em = require('discord.js').MessageEmbed;
+    if (!args[0]) {
+        message.channel.send(
+            new em()
+                .setColor('#f7b2d9')
+                .setDescription('Uh oh!')
+                .setFooter('Invalid member specified.\nUsage: `a!say <text>`'),
+        );
+    }
     if (message.guild.me.hasPermission('MANAGE_MESSAGES')) { message.delete(); }
     await message.channel.send(
         new em()
