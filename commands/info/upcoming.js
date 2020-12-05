8244323deaed9ebd202a0b3123413c5218e302fa
geixco <68980781+geixco@users.noreply.github.com>
2020-12-05 17:46:46 -0500

    Changed to v4say

--- commands/info/upcoming.js
+++ commands/info/upcoming.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message) => {
     message.channel.send(
         new em()
             .setTitle('v4 Development Progress')
-            .addField('Implemented Features', 'v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v3say')
+            .addField('Implemented Features', 'v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say')
             .addField('Planned Features', 'nekos (local db), mute, warn, softban, promote, demote, v3commands, v4help, welcomer, v4ping, music, cleanchangelog')
             .setFooter(`Astolfo ${config.version}`),
     );
