f483a099e816739394c5d35085f92af50fafefbe
geixco <68980781+geixco@users.noreply.github.com>
2020-12-06 01:33:48 -0500

    Shortened code (MessageEmbed only used once)

--- commands/info/upcoming.js
+++ commands/info/upcoming.js
@@ -1,8 +1,7 @@
 module.exports.run = async (client, message) => {
-    const em = require('discord.js').MessageEmbed,
-        config = require('../../config/config.json');
+    const config = require('../../config/config.json');
     message.channel.send(
-        new em()
+        new (require('discord.js').MessageEmbed)()
             .setTitle('v4 Development Progress')
             .addField('Implemented Features', 'v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say')
             .addField('Planned Features', 'command flags, nick, purge, nekos (local db), mute, warn, softban, promote, demote, v3commands, v4help, welcomer, v4ping, music, cleanchangelog, osu')
