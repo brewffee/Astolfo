6b3a90a6b6da7b690c60092a9807564826bfd4fa
geixco <68980781+geixco@users.noreply.github.com>
2020-12-05 23:16:32 -0500

    Added 'flags' as an upcoming feature

--- commands/info/upcoming.js
+++ commands/info/upcoming.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message) => {
         new em()
             .setTitle('v4 Development Progress')
             .addField('Implemented Features', 'v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say')
-            .addField('Planned Features', 'nick, purge, nekos (local db), mute, warn, softban, promote, demote, v3commands, v4help, welcomer, v4ping, music, cleanchangelog, osu')
+            .addField('Planned Features', 'command flags, nick, purge, nekos (local db), mute, warn, softban, promote, demote, v3commands, v4help, welcomer, v4ping, music, cleanchangelog, osu')
             .setFooter(`Astolfo ${config.version}`),
     );
 
