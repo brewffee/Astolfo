dfba6616a1add1d5b8380c1a52e5549fcd3e4b2b
geixco <68980781+geixco@users.noreply.github.com>
2020-12-06 01:30:50 -0500

    Shortened code (MessageEmbed only needed once)

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -1,6 +1,5 @@
 module.exports.run = async (client, message) => {
-    const em = require('discord.js').MessageEmbed,
-        ms = require('ms'),
-        embed = new em().setColor('#f7b2d9');
-    message.channel.send(embed.setDescription(`It has been **${ms(client.uptime, { long: true })}** since the last restart`));
+    const ms = require('ms'),
+        e = (new (require('discord.js').MessageEmbed)).setColor('#f7b2d9');
+    message.channel.send(e.setDescription(`It has been **${ms(client.uptime, { long: true })}** since the last restart`));
 };
