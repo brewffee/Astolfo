415e6443d7a6109b6ac315839ed5041d559f001d
geixco <68980781+geixco@users.noreply.github.com>
2020-12-07 10:44:16 -0500

    Removed unneeded declarations

--- events/message.js
+++ events/message.js
@@ -1,16 +1,14 @@
 module.exports = (client, message) => {
 	const { prefix } = require('../config/config.json'),
 		// { con } = require('../config/language.json'),
-		msg = message.content,
-		args = msg.slice(prefix.length).trim().split(/ +/g),
-		commandName = args.shift().toLowerCase(),
-		command = client.commandMap.get(commandName);
-	if (message.author.bot || msg.indexOf(prefix) !== 0 || !command) return;
+		args = message.content.slice(prefix.length).trim().split(/ +/g),
+		command = client.commandMap.get(args.shift().toLowerCase());
+	if (message.author.bot || !message.content.indexOf(prefix) === 0 || !command) return;
 	// Debug purposes only
 	// if (args.join(' ').length < 1) {
-	// 	console.log(`${con.R}Request: "${commandName}"`);
+	// 	console.log(`${con.R}Request: "${args.shift().toLowerCase(),}"`);
 	// } else {
-	// 	console.log(`${con.R}Request: "${commandName}", "${args.join(' ')}"`);
+	// 	console.log(`${con.R}Request: "${args.shift().toLowerCase(),}", "${args.join(' ')}"`);
 	// }
 	command.run(client, message, args);
 };
