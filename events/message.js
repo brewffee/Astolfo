3b53cd7e3030002884acf71654450dc9441ccb28
geixco <68980781+geixco@users.noreply.github.com>
2020-12-08 14:28:46 -0500

    Allow debug mode to be toggled via config

--- events/message.js
+++ events/message.js
@@ -1,14 +1,17 @@
 module.exports = (client, message) => {
-	const { prefix } = require('../config/config.json'),
-		// { con } = require('../config/language.json'),
-		args = message.content.slice(prefix.length).trim().split(/ +/g),
+	const config = require('../config/config.json'),
+		args = message.content.slice(config.prefix.length).trim().split(/ +/g),
 		command = client.commandMap.get(args.shift().toLowerCase());
-	if (message.author.bot || !message.content.indexOf(prefix.toLowerCase()) === 0 || !command) return;
-	// Debug purposes only
-	// if (args.join(' ').length < 1) {
-	// 	console.log(`${con.R}Request: "${args.shift().toLowerCase(),}"`);
-	// } else {
-	// 	console.log(`${con.R}Request: "${args.shift().toLowerCase(),}", "${args.join(' ')}"`);
-	// }
+	if (message.author.bot || !message.content.indexOf(config.prefix.toLowerCase()) === 0 || !command) return;
+
+	if (config.debug === true) {
+		const { con } = require('../config/language.json');
+		if (args.join(' ').length < 1) {
+			console.log(`${con.R}Request: "${args.shift().toLowerCase()}"`);
+		} else {
+			console.log(`${con.R}Request: "${args.shift().toLowerCase()}", "${args.join(' ')}"`);
+		}
+	}
+
 	command.run(client, message, args);
 };
