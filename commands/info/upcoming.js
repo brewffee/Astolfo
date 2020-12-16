bb32d9df2f71cae6746d218275b053a0094a6d01
geixco <68980781+geixco@users.noreply.github.com>
2020-12-16 12:15:29 -0500

    Corrected upcoming features

--- commands/info/upcoming.js
+++ commands/info/upcoming.js
@@ -3,8 +3,8 @@ module.exports.run = async (client, message) => {
     message.channel.send(
         new (require('discord.js').MessageEmbed)()
             .setTitle('v4 Development Progress')
-            .addField('Implemented Features', 'softban, unban, v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say')
-            .addField('Planned Features', 'reminders, command flags, nick, purge, nekos (local db), mute, warn, softban, promote, demote, v3commands, v4help, welcomer, v4ping, music, cleanchangelog, osu')
+            .addField('Implemented Features', 'softban, unban, v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say, v4ping, cleanchangelog')
+            .addField('Planned Features', 'reminders, command flags, nick, purge, nekos (local db), mute, warn, promote, demote, v3commands, v4help, welcomer, music, osu')
             .setFooter(`Astolfo ${config.version}`),
     );
 
