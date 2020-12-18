a309ce38b3d94af4c883bbc6ffb6c59cd429844e
geixco <68980781+geixco@users.noreply.github.com>
2020-12-18 18:26:25 -0500

    Touched up ping command

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,13 +1,12 @@
-module.exports.run = async (client, message) => {
+module.exports.run = async (client, message, args) => {
     message.channel.send(
         (new (require('discord.js').MessageEmbed))
-            .setColor('#f7b2d9')
-            .setDescription(`Pingng... 📡`),
-        ).then(m => {
-            m.edit(
-                (new (require('discord.js').MessageEmbed))
+            .setDescription(`Pinging... 📡`),
+    ).then(m => {
+        m.edit(
+            (new (require('discord.js').MessageEmbed))
                 .setColor('#f7b2d9')
                 .setDescription(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms, heartbeat ${client.ws.ping}ms.`),
-            )
-        });
+        )
+    });
 };
