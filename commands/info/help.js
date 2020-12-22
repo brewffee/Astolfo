943d4d04791502b2f18739b8477a9c7b96b1589c
geixco <68980781+geixco@users.noreply.github.com>
2020-12-22 14:14:30 -0500

    one line help :)

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,4 +1,3 @@
 module.exports.run = async (client, message) => {
-    let cmds = await JSON.stringify(Array.from(client.cmds));
-    message.channel.send(cmds);
+    const c = await JSON.stringify(Array.from(client.cmds)); message.channel.send(c);
 };
