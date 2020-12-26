2ef773e26bc4daae12c9a64ed3c9e9fedd9e7356
geixco <68980781+geixco@users.noreply.github.com>
2020-12-26 17:07:17 -0500

    Fix ban & replace hasPermission w/ permissions.has

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -7,32 +7,31 @@ module.exports.run = async (message, args) => {
     if (!message.guild && message.channel.type === 'dm') {
         errorEmbed.setDescription('You\'re trying to use a guild-only command in a DM!');
         return message.channel.send(errorEmbed);
-    } else if (!message.guild.member(message.author).hasPermission('BAN_MEMBERS')) {
+    } else if (!message.guild.member(message.author).permissions.has('BAN_MEMBERS')) {
         errorEmbed.setDescription('You do not have permission to ban members!');
         return message.channel.send(errorEmbed);
-    } else if (!message.guild.me.hasPermission('BAN_MEMBERS')) {
+    } else if (!message.guild.me.permissions.has('BAN_MEMBERS')) {
         errorEmbed.setDescription('I don\'t have permission to ban members!');
         return message.channel.send(errorEmbed);
     }
     // Define changing variables
-    let memberID, isGlobal, toBan,
-        banReason = args.join(' ').replace(args[0], '');
-    // Create the ban embed
-    const banEmbed = new (require('discord.js').MessageEmbed)()
-        .setColor('#f7b2d9')
-        .setTitle('Member successfully banned.')
-        .setDescription(`Banned ${toBan} from the server.\n\`\`\`Reason: ${banReason}\`\`\``)
-        .setFooter(`Moderator: ${message.author.tag}`, message.author.displayAvatarURL());
+    let memberID, isGlobal, toBan;
+
+    // Check for a banReason to associate the ban with
+    let banReason; args[1] ? banReason = args.join(' ').replace(args[0], '') : banReason = 'No reason provided';
+
     // Check for a GuildMember/User
     if (!args[0]) {
         // Check if no arguments were given
         return message.channel.send(errorEmbed.setDescription('Invalid usage.\nUsage: `a!ban <member> [reason]`'));
-    } else if (args[0].startsWith('<@') && args[0].endsWith('>') && args[0].replace(/[^0-9]/g, '').length == 18) {
+    } else if (/<@!?(\d)>/.test(args[0])) {
         // Check if given argument is a mention
-        memberID = args[0].toString().replace(/[^0-9]/g, '');
-    } else if (!args[0].startsWith('<@') && args[0].length == 18 && /[0-9]+$/.test(args[0])) {
+        memberID = args[0].toString().replace(/\D/g, '');
+        message.channel.send(new (require('discord.js')).MessageEmbed().setDescription(memberID));
+    } else if (/[0-9]+$/.test(args[0])) {
         // Check if given argument is a user ID
         memberID = args[0].toString();
+        message.channel.send(new (require('discord.js')).MessageEmbed().setDescription(memberID));
     } else {
         // Check if given argument does not match ID or mention format
         return message.channel.send(errorEmbed.setDescription('Invalid user specified.\nUsage: `a!ban <member> [reason]`'));
@@ -48,18 +47,13 @@ module.exports.run = async (message, args) => {
         }
     }
 
-    // Check for a banReason to associate the ban with
-    if (banReason.length < 1) {
-        banReason = 'No reason provided';
-    }
-
     // Checks (if GuildMember is present)
     if (!isGlobal) {
         if (toBan == message.author.id) {
             return message.channel.send(errorEmbed.setDescription('You can\'t ban yourself!'));
         } else if (toBan == message.guild.me.id) {
             return message.channel.send(errorEmbed.setDescription('I can\'t ban myself!'));
-        } else if (!message.guild.member(toBan).bannable || message.guild.member(toBan).hasPermission('ADMINISTRATOR')) {
+        } else if (!message.guild.member(toBan).bannable || message.guild.member(toBan).permissions.has('ADMINISTRATOR')) {
             return message.channel.send(errorEmbed.setDescription('The specified member is immune to bans!'));
         }
     }
@@ -74,11 +68,18 @@ module.exports.run = async (message, args) => {
     if (!isGlobal) {
         // Attempt to ban the GuildMember, send error if failed.
         try {
-            message.guild.members.ban(memberID, { days: 7, reason: banReason });
+            // message.guild.members.ban(memberID, { days: 7, reason: banReason });
         } catch (error) {
             console.error;
             message.channel.send(errorEmbed.setDescription('An unknown error occured whilst trying to run that command! Please try again in a few seconds.'));
         }
+        // Create the ban embed
+        const banEmbed = new (require('discord.js').MessageEmbed)()
+            .setColor('#f7b2d9')
+            .setTitle('Member successfully banned.')
+            .setDescription(`Banned ${toBan} from the server.\n\`\`\`Reason: ${banReason}\`\`\``)
+            .setFooter(`Moderator: ${message.author.tag}`, message.author.displayAvatarURL());
+
         // Send the ban embed
         return message.channel.send(banEmbed);
     } else {
@@ -90,12 +91,19 @@ module.exports.run = async (message, args) => {
         }
         // Attempt to ban the user, send error if failed.
         try {
-            message.guild.members.ban({ user: memberID, days: 7, reason: banReason });
+            // message.guild.members.ban({ user: memberID, days: 7, reason: banReason });
         } catch (error) {
             message.channel.send('Triggered isGlobal error'),
                 console.error,
                 message.channel.send(errorEmbed.setDescription('An unknown error occured whilst trying to run that command! Please try again in a few seconds.'));
         }
+        // Create the ban embed
+        const banEmbed = new (require('discord.js').MessageEmbed)()
+            .setColor('#f7b2d9')
+            .setTitle('Member successfully banned.')
+            .setDescription(`This user will no longer be able to join the server.\n\`\`\`Reason: ${banReason}\`\`\``)
+            .setFooter(`Moderator: ${message.author.tag}`, message.author.displayAvatarURL());
+
         // Send the embed
         return message.channel.send(banEmbed);
     }
