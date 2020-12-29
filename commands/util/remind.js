5a17411c338152bfd18931f9ec648c1e4b66d0e3
geixco <68980781+geixco@users.noreply.github.com>
2020-12-28 23:24:53 -0500

    Remind command is now useable

--- commands/util/remind.js
+++ commands/util/remind.js
@@ -1,14 +1,14 @@
 module.exports.run = async (message, args) => {
     const ms = require('ms'),
-        time = ms(args[1]);
+        time = ms(args[0]);
     // current time + time = expiry date
     // send data to db
     // TEMPORARY METHOD
-    message.channel.send(`request: send '${args[1]}' in ${args[0]} [ms: ${ms(args[0])}]`);
+    message.channel.send(`request: send '${args.slice(1).join(' ')}' in ${args[0]} [ms: ${ms(args[0])}]`);
     if (!time) {
         message.channel.send('Please provide a correct time format!');
     } else if (time < ms('5 minutes')) {
-        setTimeout(() => { message.channel.send(args[1]); }, time);
+        setTimeout(() => { message.channel.send(args.slice(1).join(' ')); }, time);
     } else {
         // 500ms interval to check if time is reached
     }
