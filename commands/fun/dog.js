075ec2f5bdc96133cd54beafdf68398f62381a2c
geixco <68980781+geixco@users.noreply.github.com>
2020-12-29 19:22:33 -0500

    Update dog.js

--- commands/fun/dog.js
+++ commands/fun/dog.js
@@ -1,39 +1,31 @@
 module.exports.run = async (message) => {
-  const fetch = require("node-fetch"),
-    auth = require("../../auth/auth.json");
-  fetch(
-    `https://api.tenor.com/v1/random?key=${auth.api.key.tenor}&locale=en_US&q=dog&limit=1`
-  )
-    .then((r) => r.json())
-    .then((q) =>
-      message.channel.send(
-        new (require("discord.js").MessageEmbed)()
-          .setColor("#f7b2d9")
-          .setTitle("Woof! :dog2:")
-          .setImage(q.results[0].media[0].gif.url)
-          .setFooter(`Requested by ${message.author.username}`)
-      )
-    )
-    .catch((e) => {
-      console.log(e);
-      if (
-        e
-          .toString()
-          .startsWith("TypeError: Cannot read property 'media' of undefined")
-      ) {
-        return message.channel.send(
-          new (require("discord.js").MessageEmbed)()
-            .setColor("#f7b2d9")
-            .setTitle("Uh oh!")
-            .setDescription("Unable to find a puppy!")
-        );
-      } else {
-        return message.channel.send(
-          new (require("discord.js").MessageEmbed)()
-            .setColor("#f7b2d9")
-            .setTitle("Uh oh!")
-            .setDescription("An error occured whilst running this command!")
-        );
-      }
-    });
+    require("node-fetch")(`https://api.tenor.com/v1/random?key=${require("../../auth/auth.json").api.key.tenor}&locale=en_US&q=dog&limit=1`)
+        .then((r) => r.json())
+        .then((q) =>
+            message.channel.send(
+                new (require("discord.js").MessageEmbed)()
+                    .setColor("#f7b2d9")
+                    .setTitle("Woof! :dog2:")
+                    .setImage(q.results[0].media[0].gif.url)
+                    .setFooter(`Requested by ${message.author.username}`)
+            )
+        )
+        .catch((e) => {
+            console.log(e);
+            if (e.toString().startsWith("TypeError: Cannot read property 'media' of undefined")) {
+                return message.channel.send(
+                    new (require("discord.js").MessageEmbed)()
+                        .setColor("#f7b2d9")
+                        .setTitle("Uh oh!")
+                        .setDescription("Unable to find a puppy!")
+                );
+            } else {
+                return message.channel.send(
+                    new (require("discord.js").MessageEmbed)()
+                        .setColor("#f7b2d9")
+                        .setTitle("Uh oh!")
+                        .setDescription("An error occured whilst running this command!")
+                );
+            }
+        });
 };
