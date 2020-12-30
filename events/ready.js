d797a7ccfe1c1e9e5ff9922b3caff647d5f9315d
geixco <68980781+geixco@users.noreply.github.com>
2020-12-29 21:50:36 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,10 +5,7 @@ module.exports = (client) => {
   client.user.setActivity("fate", {
     type: "WATCHING",
   });
-  client.guilds.cache
-    .get("761203866732724225")
-    .channels.cache.get("787087630390919228")
-    .send(
+  client.guilds.cache.get("761203866732724225").channels.cache.get("787087630390919228").send(
       new Discord.MessageEmbed()
         .setTitle("Astolfo is online!")
         .setDescription(`Listening on ${client.guilds.cache.size} guilds!`)
@@ -17,7 +14,5 @@ module.exports = (client) => {
     );
   console.log(`${con.OK}${net.complete}`);
   console.log(`${con.OK}${ev.ready[0]}${config.version}${ev.ready[1]}`);
-  console.log(
-    `${con.OK}${ev.listening[0]}${client.guilds.cache.size}${ev.listening[1]}${con.LINE}`
-  );
+  console.log(`${con.OK}${ev.listening[0]}${client.guilds.cache.size}${ev.listening[1]}${con.LINE}`);
 };
