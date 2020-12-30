45bdcfa62bb4da2db2695ebe0edbce412e2064d3
geixco <68980781+geixco@users.noreply.github.com>
2020-12-29 21:49:21 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,26 +1,14 @@
 module.exports = (client, message) => {
   const config = require("../config/config.json"),
-    args = message.content
-      .slice(config.prefix.length)
-      .trim()
-      .split(/ +/g)
-      .filter((a) => !/^--(.*)/.test(a)),
+    args = message.content.slice(config.prefix.length).trim().split(/ +/g).filter((a) => !/^--(.*)/.test(a)),
     command = client.cmds.get(args.shift().toLowerCase()),
-    flags = message.content
-      .toLowerCase()
-      .split(/ +/g)
-      .reduce((flags, flag) => {
+    flags = message.content.toLowerCase().split(/ +/g).reduce((flags, flag) => {
         const match = /^--([^=]+)(?:=(.*))?/.exec(flag);
         if (match) flags[match[1]] = match[2] ?? true;
         return flags;
       }, {});
-  if (
-    message.author.bot ||
-    !message.content.indexOf(config.prefix.toLowerCase()) === 0 ||
-    !command
-  )
-    return;
-
+  if (message.author.bot || !message.content.indexOf(config.prefix.toLowerCase()) === 0 || !command) return;
+        
   if (config.debug) {
     const { con } = require("../config/language.json");
     let cmd;
@@ -32,5 +20,6 @@ module.exports = (client, message) => {
       console.log(`${con.R}Request: "${cmd}", "${args.join(" ")}"`);
     }
   }
+
   command.run(message, args, flags);
 };
