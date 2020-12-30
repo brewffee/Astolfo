b02b82386792960b2066b76f8a544db88698f4f3
geixco <68980781+geixco@users.noreply.github.com>
2020-12-29 19:57:38 -0500

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -4,9 +4,7 @@ module.exports.run = async (message) => {
     new (require("discord.js").MessageEmbed)()
       .setColor("#f7b2d9")
       .setTitle("Click here to invite me to your server!")
-      .setURL(
-        "https://discord.com/api/oauth2/authorize?client_id=727367942525681704&scope=bot"
-      )
+      .setURL("https://discord.com/api/oauth2/authorize?client_id=727367942525681704&scope=bot")
       .setFooter(`Astolfo ${config.version}`)
   );
 };
