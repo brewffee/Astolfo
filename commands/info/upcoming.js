92e07962ae0b66764df6244336b27ead62c38271
geixco <68980781+geixco@users.noreply.github.com>
2020-12-29 20:01:20 -0500

    Update upcoming.js

--- commands/info/upcoming.js
+++ commands/info/upcoming.js
@@ -5,11 +5,11 @@ module.exports.run = async (message) => {
       .setTitle("v4 Development Progress")
       .addField(
         "Implemented Features",
-        "softban, unban, v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say, v4ping, cleanchangelog"
+        "reminders, flags, softban, unban, v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say, v4ping, cleanchangelog"
       )
       .addField(
         "Planned Features",
-        "reminders, command flags, nick, purge, nekos (local db), mute, warn, promote, demote, v3commands, v4help, welcomer, music, osu, hentai"
+        "nick, purge, nekos (local db), mute, warn, promote, demote, v3commands, v4help, welcomer, music, osu, hentai"
       )
       .setFooter(`Astolfo ${config.version}`)
   );
