d12bd676b270ed8dcedae8e721de743c03078cd5
geixco <68980781+geixco@users.noreply.github.com>
2020-12-31 00:01:51 -0500

    Made flag non-assignable + limited flags to 'dm'

--- commands/util/remind.js
+++ commands/util/remind.js
@@ -5,7 +5,7 @@ module.exports.run = async (message, args, flags) => {
         errorEmbed = new (require('discord.js').MessageEmbed)()
         .setColor('#f7b2d9')
         .setTitle('Uh oh!');
-    if (!time) {
+    if (!time || flags['dm']?.length >= 0 || (flags.size != 0 && !flags.dm)) {
         message.channel.send(errorEmbed.setDescription('Invalid usage.\nUsage: `a!remind <time> [--dm] <message>`'));
     } else if (time < 2147483647 && time > 999) {
         const setEmbed = new (require('discord.js')).MessageEmbed()
@@ -13,12 +13,14 @@ module.exports.run = async (message, args, flags) => {
             .setTimestamp(Date.now() + time);
         if (config.debug) {
             message.channel.send(
-                setEmbed.addField('Input',
+                setEmbed.addField(
+                    'Input',
                     `\`\`\`Message: ${message.content}\nReminder: ${args.slice(1).join(' ').replace(/`/g, '`\u200b')}\nTime: ${args[0]} [ms: ${ms(args[0])}]\nType: ${flags.dm ? 'DM' : 'Channel'}\`\`\``),
             );
         } else {
             message.channel.send(setEmbed);
         }
+
         setTimeout(() => { flags.dm ? message.author.send(args.slice(1).join(' ')) : message.channel.send(args.slice(1).join(' '), { allowedMentions: { everyone: false, users: [message.author.id] } }); }, time);
     } else {
         message.channel.send(errorEmbed.setDescription('Time must be more than `1s` and less than `25d`\nUsage: `a!remind <time> [--dm] <message>`'));
