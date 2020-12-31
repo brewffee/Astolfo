ef93c1bfc3375e78e0eb08cb62047b960bf8c753
geixco <68980781+geixco@users.noreply.github.com>
2020-12-31 00:02:07 -0500

    Added some form of flag integration

--- commands/info/changelog.js
+++ commands/info/changelog.js
@@ -1,51 +1,46 @@
 module.exports.run = async (message, args, flags) => {
-  const config = require("../../config/config.json"),
-    changelog = require("../../logs/entries.json"),
-    latest = "b168";
-  message.channel.send(
-    `version ${config.version.substr(config.version.indexOf("b"))}, args ${
-      args[0]
-    }, latest ${latest}`
-  );
+  const config = require('../../config/config.json'),
+    changelog = require('../../logs/entries.json'),
+    latest = 'b181';
   try {
     message.channel.send(
-      new (require("discord.js").MessageEmbed)()
+      new (require('discord.js')).MessageEmbed()
         .setTitle(changelog.entry[args[0]].date)
         .addField(changelog.entry[args[0]].head, changelog.entry[args[0]].body)
-        .setFooter(`Astolfo ${config.version}`)
+        .setFooter(`Astolfo ${config.version}`),
     );
   } catch {
-    if (!args[0] || args[0] == "latest") {
+    if ((!args[0] && !flags['latest']) || flags.latest) {
       message.channel.send(
-        new (require("discord.js").MessageEmbed)()
+        new (require('discord.js').MessageEmbed)()
           .setTitle(changelog.entry[latest].date)
           .addField(changelog.entry[latest].head, changelog.entry[latest].body)
-          .setFooter(`Astolfo ${config.version}`)
+          .setFooter(`Astolfo ${config.version}`),
       );
     } else if (
-      args[0] == config.version.substr(config.version.indexOf("b")) &&
+      args[0] == config.version.substr(config.version.indexOf('b')) &&
       config.version != latest
     ) {
       message.channel.send(
-        new (require("discord.js").MessageEmbed)()
-          .setTitle("Uh oh!")
+        new (require('discord.js').MessageEmbed)()
+          .setTitle('Uh oh!')
           .setDescription(
-            "This version is a testing release and has not been documented yet."
-          )
+            'This version is a testing release and has not been documented yet.',
+          ),
       );
     } else if (args[1] || !/^b\d{3}$/.test(args[0])) {
       message.channel.send(
-        new (require("discord.js").MessageEmbed)()
-          .setTitle("Uh oh!")
-          .setDescription("Invalid usage")
+        new (require('discord.js').MessageEmbed)()
+          .setTitle('Uh oh!')
+          .setDescription('Invalid usage'),
       );
     } else {
       message.channel.send(
-        new (require("discord.js").MessageEmbed)()
-          .setTitle("Uh oh!")
+        new (require('discord.js').MessageEmbed)()
+          .setTitle('Uh oh!')
           .setDescription(
-            "The given version is either invalid, undocumented, or unreleased."
-          )
+            'The given version is either invalid, undocumented, or unreleased.',
+          ),
       );
     }
   }
