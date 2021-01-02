dd4605799eb8d87169c08b6186d9d24955f2e8de
geixco <68980781+geixco@users.noreply.github.com>
2021-01-01 20:42:07 -0500

    Formatting change

--- commands/admin/-emit.js
+++ commands/admin/-emit.js
@@ -1,5 +1,5 @@
 module.exports.run = async (message, args) => {
-  if (message.author.id !== "548675133481418752") return;
+  if (message.author.id !== '548675133481418752') return;
   try {
     message.client.emit(args[0], message);
     message.channel.send(`EVENT type [${args[0]}] emitted`, { code: true });
