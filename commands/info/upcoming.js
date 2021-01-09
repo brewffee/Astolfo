f8c2f02c989fcdc96b580a82658cd99b950e146c
geixco <68980781+geixco@users.noreply.github.com>
2021-01-09 06:44:19 -0500

    Rework `upcoming` command

--- commands/info/upcoming.js
+++ commands/info/upcoming.js
@@ -1,15 +1,12 @@
 module.exports.run = async (message) => {
   const config = require('../../config/config.json');
+  const features = ['- nick', '- purge', '- mute', '- warn', '- promote', '- demote', '- v3commands', '- v4help', '- welcomer', '- music', '- osu', '- hentai', '- anime gifs'];
   message.channel.send(
     new (require('discord.js').MessageEmbed)()
-      .setTitle('v4 Development Progress')
-      .addField(
-        'Implemented Features',
-        'nekos, reminders, flags, softban, unban, v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say, v4ping, cleanchangelog',
-      )
+      .setTitle('v4 Upcoming Features')
       .addField(
         'Planned Features',
-        'nick, purge, mute, warn, promote, demote, v3commands, v4help, welcomer, music, osu, hentai, gif commands',
+        features.join('\n'),
       )
       .setFooter(`Astolfo ${config.version}`),
   );
