e8076acc2777614f60ecb484da8497d0afc25314
geixco <68980781+geixco@users.noreply.github.com>
2021-01-09 06:25:03 -0500

    Added 'gif commands' as planned feature

--- commands/info/upcoming.js
+++ commands/info/upcoming.js
@@ -5,11 +5,11 @@ module.exports.run = async (message) => {
       .setTitle('v4 Development Progress')
       .addField(
         'Implemented Features',
-        'reminders, flags, softban, unban, v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say, v4ping, cleanchangelog',
+        'nekos, reminders, flags, softban, unban, v4ban, v4kick, v4gif (generic, cat, dog), upcoming, changelog, v3avatar, v3invite, v4say, v4ping, cleanchangelog',
       )
       .addField(
         'Planned Features',
-        'nick, purge, nekos (local db), mute, warn, promote, demote, v3commands, v4help, welcomer, music, osu, hentai',
+        'nick, purge, mute, warn, promote, demote, v3commands, v4help, welcomer, music, osu, hentai, gif commands',
       )
       .setFooter(`Astolfo ${config.version}`),
   );
