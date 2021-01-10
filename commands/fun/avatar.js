5df9de5f92247ec324d8855315a37e25356eda4c
geixco <68980781+geixco@users.noreply.github.com>
2021-01-10 08:47:09 -0500

    Add new utilities to `avatar`

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -1,46 +1,27 @@
 module.exports.run = async (message, args) => {
-  const errorEmbed = new (require('discord.js').MessageEmbed)()
-    .setColor('#f7b2d9')
-    .setTitle('Uh oh!');
-  let memberID;
-  if (!args[0]) {
-    // Check if no arguments were given
-    memberID = message.author.id;
-  } else if (args[1]) {
+  const err = require('../../util/Errors.js'),
+    { getMember } = require('../../util/Member.js');
+  let member;
+  if (args[1]) {
     // Check if too many arguments were given
-    return message.channel.send(
-      errorEmbed.setDescription('Invalid usage.\nUsage: `a!avatar [member]`'),
-    );
-  } else if (/(^<@?!)?(\d)(>$)/.test(args[0])) {
-    // Check if given argument is a mention
-    memberID = args[0].toString().replace(/[^0-9]/g, '');
-  } else if (/\d+$/.test(args[0])) {
-    // Check if given argument is a user ID
-    memberID = args[0].toString();
-  } else {
-    // Check if given argument does not match ID or mention format
-    return message.channel.send(
-      errorEmbed.setDescription('Invalid member.\nUsage: `a!avatar [member]`'),
-    );
+    return err.throw('AvatarUsage', message.channel);
   }
+
+  args[0] ? member = await getMember(args[0], message.guild) : member = message.author.id;
 
   // Find a GuildMember if all checks passed
   let foundMember;
   try {
-    foundMember = await message.guild.members.fetch({ user: memberID, force: true, cache: false });
+    foundMember = await message.guild.members.fetch({ user: member.id, force: true, cache: false });
   } catch (E) {
     if (E.message === 'Unknown Member') {
-      return message.channel.send(
-        errorEmbed.setDescription(
-          'I can\'t get the avatar of a someone who isn\'t in the server!',
-        ),
-      );
+      return err.throw('AvatarMember', message.channel);
     }
   }
 
   // Retrieve and send the avatar
   message.channel.send(
-    new (require('discord.js').MessageEmbed)()
+    new (require('discord.js')).MessageEmbed()
       .setColor('#f7b2d9')
       .setImage(`${foundMember.user.displayAvatarURL({ size: 4096, dynamic: true })}`)
       .setTitle(`Avatar of ${foundMember.user.username}`),
