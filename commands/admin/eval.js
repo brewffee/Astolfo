5d13c431625b26cbe12e859c41bae96d0ad0fae5
geixco <68980781+geixco@users.noreply.github.com>
2021-01-15 11:13:38 -0500

    wow

--- commands/admin/eval.js
+++ commands/admin/eval.js
@@ -1,12 +1,12 @@
 module.exports.run = async (message, args) => {
     if(message.author.id !== '548675133481418752') return;
     try {
-      let evaled = eval(args.join(' '));
-      typeof (evaled) !== 'string' ? evaled = require('util').inspect(evaled) : evaled = `'${evaled}'`;
-      if (evaled.toString().length > 1950) {
-        return message.channel.send(`pog \`\`\`js\n${evaled.toString().substr(0, 1950).replace(/`/g, '`\u200b').replace(/@/g, '@\u200b')}...\`\`\`\n\n (see console output)`);
+      let res = eval(args.join(' '));
+      typeof (res) !== 'string' ? res = require('util').inspect(res, { depth: 0 }) : res = `'${res}'`;
+      if (res.toString().length > 1950) {
+        return message.channel.send(`pog \`\`\`js\n${res.toString().substr(0, 1950).replace(/`/g, '`\u200b').replace(/@/g, '@\u200b')}...\`\`\`\n\n (see console output)`);
       } else {
-        return message.channel.send(`pog \`\`\`js\n${evaled.toString().replace(/`/g, '`\u200b').replace(/@/g, '@\u200b')}\`\`\``);
+        return message.channel.send(`pog \`\`\`js\n${res.toString().replace(/`/g, '`\u200b').replace(/@/g, '@\u200b')}\`\`\``);
       }
     } catch (err) {
       return message.channel.send(`what the fuck \`\`\`js\n${err.toString().replace(/`/g, '`\u200b').replace(/@/g, '@\u200b')}\n\`\`\``);
