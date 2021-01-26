40f02fee12a8403018578731fa682ada18163ef9
geixco <68980781+geixco@users.noreply.github.com>
2021-01-25 21:30:30 -0500

    yoinked

--- commands/admin/eval.js
+++ commands/admin/eval.js
@@ -1,5 +1,4 @@
 module.exports.run = async (message, args, flags) => {
-  if (message.content.includes('client.token')) return message.channel.send('no');
   if (message.author.id !== '548675133481418752') {
     return message.channel.send(
       new (require('discord.js')).MessageEmbed()
@@ -12,7 +11,7 @@ module.exports.run = async (message, args, flags) => {
     let res = flags.sync ? eval(args.join(' ')) : await eval(`( async () => { return ${args.join(' ')}; })()`);
     typeof (res) !== 'string' ? res = require('util').inspect(res, { depth: 0 }) : res = `'${res}'`;
     if (res.toString().length > 1950) {
-      const bin = await (require('sourcebin')).create([{ name: 'eval', content: res.toString(), languageID: 'javascript' }]);
+      const bin = await (require('sourcebin')).create([{ name: 'eval', content: res.toString().replace(message.client.token, 'no'), languageID: 'javascript' }]);
       return message.channel.send(
         new (require('discord.js')).MessageEmbed()
           .setTitle('Eval output is too large!')
@@ -22,7 +21,7 @@ module.exports.run = async (message, args, flags) => {
       message.channel.send(
         new (require('discord.js')).MessageEmbed()
           .setTitle('Eval success!')
-          .setDescription(`\`\`\`js\n${res.toString().replace(/`/g, '`\u200b')}\`\`\``)
+          .setDescription(`\`\`\`js\n${res.toString().replace(/`/g, '`\u200b').replace(message.client.token, 'no')}\`\`\``)
           .setFooter(`Took ${parseFloat((process.hrtime(start)[1] / 1000000).toFixed(2))}ms`),
       );
     }
