4b3a11061d466459392257cb24bc1adcf44ec90a
geixco <68980781+geixco@users.noreply.github.com>
2021-02-22 15:35:27 -0500

    Readd the NSFW check lol

--- commands/nsfw/hentai.js
+++ commands/nsfw/hentai.js
@@ -3,6 +3,7 @@ module.exports.run = async (message, args) => {
   const Discord = require('discord.js');
   try {
     if (args[1]) null; // Errors.throw('HentaiUsage', message.channel);
+    if (!message.channel.nsfw) return Errors.throw('NSFWOnly', message.channel);
     const { image } = (await (await require('node-fetch')('http://api.nekos.fun:8080/api/hentai')).json());
     return message.channel.send(
       new Discord.MessageEmbed()
