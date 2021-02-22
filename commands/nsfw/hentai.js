adc749b943fbe98ba47984cac60d8c10cc7a5c20
geixco <68980781+geixco@users.noreply.github.com>
2021-02-22 11:10:33 -0500

    Clean up hentai command

--- commands/nsfw/hentai.js
+++ commands/nsfw/hentai.js
@@ -1,19 +1,19 @@
-module.exports.run = async (message) => {
-    require('node-fetch')('http://api.nekos.fun:8080/api/hentai')
-        .then((r) => r.json())
-        .then((b) => {
-            if (message.channel.nsfw) {
-                return message.channel.send(
-                    new (require('discord.js')).MessageEmbed()
-                        .setTitle('Hentai!')
-                        .setColor(16233177)
-                        .setURL(b.image)
-                        .setImage(b.image)
-                        .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true })),
-                );
-            } else {
-                return require('../../util/Errors.js').throw('NSFWOnly', message.channel);
-            }
-
-        });
+module.exports.run = async (message, args) => {
+  const Errors = require('../../util/Errors.js');
+  const Discord = require('discord.js');
+  try {
+    if (args[1]) null; // Errors.throw('HentaiUsage', message.channel);
+    const { image } = (await (await require('node-fetch')('http://api.nekos.fun:8080/api/hentai')).json());
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setTitle('Hentai!')
+        .setColor(16233177)
+        .setURL(image)
+        .setImage(image)
+        .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true })),
+    );
+  } catch (error) {
+    console.log(error);
+    return Errors.throw('Generic', message.channel);
+  }
 };
