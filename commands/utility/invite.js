b81315be53dbb7881d448758e41701982c430c05
geixco <geixco@protonmail.com>
2022-03-21 19:01:38 -0400

    use correct invite link

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -12,7 +12,7 @@ module.exports = new Command({
       new Discord.MessageEmbed()
         .setColor('#f7b2d9')
         .setTitle('Click here to invite me to your server!')
-        .setURL('https://discord.com/api/oauth2/authorize?client_id=727367942525681704&scope=bot')
+        .setURL('https://discord.com/api/oauth2/authorize?client_id=779526520121065472&scope=bot')
         .setFooter(`Astolfo ${config.version}`),
     );
   },
